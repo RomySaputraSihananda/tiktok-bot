6d005f84b7a1fbde96eacaa856b290d16cb2e341
romys <romysaputrasihanandaa@gmail.com>
2023-12-11 20:35:11 +0700

    update

--- zefoy/Zefoy.py
+++ zefoy/Zefoy.py
@@ -1,11 +1,14 @@
+import os
 import undetected_chromedriver as uc
 from undetected_chromedriver import Chrome
-import os
-
+from time import sleep
 class Zefoy:
     def __init__(self) -> None:
         self.__url: str = 'https://zefoy.com'
         self.__driver: Chrome = uc.Chrome(driver_executable_path=f"./driver/{os.listdir('./driver')[0]}")
+        self.__driver.set_window_size(500, 800)
+        self.__sent = 1
+
         self.__selectors: dict = {
                 selector: f".row div:nth-child({i + 2}) div button" for i, selector in enumerate(['followers', 'hearts', 'chearts', 'views', 'shares', 'favorites', 'livestream'])                   
             }
@@ -16,26 +19,44 @@ class Zefoy:
                 return self.__driver.find_element("css selector", selector)
             except Exception:
                 pass
+    
+    def __send_bot(self, key: str):
+        self.__wait_element(self.__selectors[key]).click()
+        videourl = input('videourl : ')
+        self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{key}-menu div form div input').send_keys(videourl)
+        self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{key}-menu div form div div button').click()
+        
+        self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{key}-menu div div div div form button').click()
+
+        text = self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{key}-menu div div span:last-child').text
+        print(f'{self.__sent} sent {key} {text}')
+
+        self.__sent += 1
+        self.__send_bot(key)
 
     def execute(self):
         self.__driver.get(self.__url)
 
         self.__wait_element('.card.mb-3.word-load')
 
+        captha = input('Solve the Captha : ') 
+
+        self.__wait_element('.card.mb-3.word-load div input').send_keys(captha)
+        self.__wait_element('.card.mb-3.word-load div div button').click()
+
         self.__wait_element('.row')
 
-        for i, keys in enumerate(self.__selectors):
-            print(f'{i + 1}. [ {self.__wait_element(self.__selectors[keys]).is_enabled()} ] {keys}')
+        for i, key in enumerate(self.__selectors):
+            print(f'{i + 1}. [ {self.__wait_element(self.__selectors[key]).is_enabled()} ] {key}')
 
         choice = int(input('Choice : '))
 
-        for i, keys in enumerate(self.__selectors):
-            if(i == choice - 1): 
-                self.__wait_element(self.__selectors[keys]).click()
-                self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{keys}-menu div form div input').send_keys('hehehehehehe')
-            
-        self.__wait_element('#test')
+        for i, key in enumerate(self.__selectors):
+            if(i == choice - 1): self.__send_bot(key)
+                
 
+        self.__wait_element('#test')
+# https://vt.tiktok.com/ZSNVHNyVT/
 # testing
 if(__name__ == '__main__'):
     zefoy: Zefoy = Zefoy()
