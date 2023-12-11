c94f58912db4d82d8ed1b647eed3dd08cafd307a
romys <romysaputrasihanandaa@gmail.com>
2023-12-11 19:14:39 +0700

    update

--- zefoy/Zefoy.py
+++ zefoy/Zefoy.py
@@ -7,8 +7,8 @@ class Zefoy:
         self.__url: str = 'https://zefoy.com'
         self.__driver: Chrome = uc.Chrome(driver_executable_path=f"./driver/{os.listdir('./driver')[0]}")
         self.__selectors: dict = {
-            selector: f'.row div:nth-child({i + 2}) div button' for i, selector in enumerate(['followers', 'hearts', 'comment_hearts', 'views', 'shares', 'favorites', 'Live Stream'])
-        } 
+                selector: f".row div:nth-child({i + 2}) div button" for i, selector in enumerate(['followers', 'hearts', 'chearts', 'views', 'shares', 'favorites', 'livestream'])                   
+            }
 
     def __wait_element(self, selector: str):
         while True:
@@ -24,8 +24,17 @@ class Zefoy:
 
         self.__wait_element('.row')
 
-        for i, selector in enumerate(self.__selectors):
-            print(f'{i + 1}. [ {self.__wait_element(self.__selectors[selector]).is_enabled()} ] {selector}')
+        for i, keys in enumerate(self.__selectors):
+            print(f'{i + 1}. [ {self.__wait_element(self.__selectors[keys]).is_enabled()} ] {keys}')
+
+        choice = int(input('Choice : '))
+
+        for i, keys in enumerate(self.__selectors):
+            if(i == choice - 1): 
+                self.__wait_element(self.__selectors[keys]).click()
+                self.__wait_element(f'.col-sm-5.col-xs-12.p-1.container.t-{keys}-menu div form div input').send_keys('hehehehehehe')
+            
+        self.__wait_element('#test')
 
 # testing
 if(__name__ == '__main__'):
